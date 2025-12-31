cb4dd5e5356d1d9219eff94bdc3cbfdbe3e8b351
ArpitSharma-21 <arpi66.ascii@gmail.com>
2025-12-31 15:35:51 +0530

    query selector

---
+++ 104.js
@@ -0,0 +1,9 @@
+//query selector
+
+const mainheading = document.querySelector("#main-heading");
+// console.log(mainheading);
+const selecthead = document.querySelector(".header");
+// console.log(selecthead);
+const navitem = document.querySelector(".nav-item");
+console.log(navitem);
+
