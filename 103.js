6f6738be13b1c37ae024dac29c7723e53e887167
ArpitSharma-21 <arpi66.ascii@gmail.com>
2025-12-31 15:19:07 +0530

    getelementbyid()

---
+++ 103.js
@@ -0,0 +1,3 @@
+// select element by using get elemet by id
+console.dir(document.getElementById("main-heading")); // ye ek object return krega
+// console.log(typeof document.getElementById("main-heading") ); // type batayega which is object
