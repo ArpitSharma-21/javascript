1c8ad43dc3c385a4c1fd247a684453287543c2bf
ArpitSharma-21 <arpi66.ascii@gmail.com>
2026-01-03 11:45:56 +0530

    htmlcollection and nodelist

---
+++ 108.js
@@ -0,0 +1,12 @@
+//get multiple elements using getElements by class name
+// get multiple elements items using queryselectorall
+
+const navitems = document.getElementsByClassName("nav-item") //htmlcollection
+console.log(navitems);
+console.log(navitems[0]);
+console.log(navitems[1]);
+console.log(navitems[2]);
+
+// const navitems = document.querySelectorAll(".nav-item");
+// console.log(navitems);
+// console.log(navitems[1]);
