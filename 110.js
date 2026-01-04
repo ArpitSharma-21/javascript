2e6f0a315170fc58ad141bc744b5ff21b4874021
ArpitSharma-21 <arpi66.ascii@gmail.com>
2026-01-04 10:41:44 +0530

    how innerhtml works

---
+++ 110.js
@@ -0,0 +1,6 @@
+//inner html
+const headline = document.querySelector(".headline");
+// console.log(headline.innerHTML);
+headline.innerHTML="<p> inner html is changed </p> ",
+headline.innerHTML += " <button>learn more</button> ";
+console.log(headline.innerHTML);
