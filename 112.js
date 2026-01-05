6d0e5292250310a6344d566d535496f3dd9c23aa
ArpitSharma-21 <arpi66.ascii@gmail.com>
2026-01-05 17:05:35 +0530

    toggle,classlist and methods

---
+++ 112.js
@@ -0,0 +1,13 @@
+const sectiontodo = document.querySelector(".section-todo");
+// console.log(sectiontodo.classList);
+// sectiontodo.classList.add('bg-dark');
+// sectiontodo.classList.remove("container");
+
+//check class exist or not
+// const ans = sectiontodo.classList.contains("container");
+// console.log(ans);
+
+
+// toggle add if not presennt if present then remove
+// sectiontodo.classList.toggle("bg-dark");
+// sectiontodo.classList.toggle("bg-dark");
