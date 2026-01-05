6c82853749b67b240f533fa9b2581d255edb3c7a
ArpitSharma-21 <arpi66.ascii@gmail.com>
2026-01-05 17:07:21 +0530

    innerhtml , html element

---
+++ 113.js
@@ -0,0 +1,5 @@
+//inNERhtml to add html element
+
+const todolist = document.querySelector(".todo-list");
+todolist.innerHTML += "<li>new todo</li>";
+todolist.innerHTML += "<li>teach students </li> ";
