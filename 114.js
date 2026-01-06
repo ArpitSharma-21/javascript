ac5137505943d4981f84f8dd95f7ab8073755871
ArpitSharma-21 <arpi66.ascii@gmail.com>
2026-01-06 16:08:55 +0530

    append,prepend,remove

---
+++ 114.js
@@ -0,0 +1,26 @@
+// document.createElement()
+// append
+// prepend
+// remove
+
+// const newtodoitem = document.createElement("li");
+// newtodoitem.textContent="teach students";
+// const todolist = document.querySelector(".todo-list");
+// // todolist.append(newtodoitem);
+// todolist.prepend(newtodoitem);
+// console.log(newtodoitem);
+
+//remove
+// const todo1 =document.querySelector('.todo-List li');
+// console.log(todo1);
+// todo1.remove();
+// console.log(todo1);
+
+
+//before after
+const newtodoitem = document.createElement("li");
+newtodoitem.textContent="teach students";
+const todolist = document.querySelector(".todo-list");
+// todolist.after(newtodoitem);
+
+// todolist.before(newtodoitem);
