dcb2542f467f9dbf01d589166cfc4c8da8a74b7d
ArpitSharma-21 <arpi66.ascii@gmail.com>
2026-01-18 17:03:29 +0530

    live vs static list

---
+++ 118.js
@@ -0,0 +1,21 @@
+// Live list 
+
+// const ul = document.querySelector(".todo-list ");
+// const listitems = ul.getElementsByTagName("li");
+
+// const sixthli = document.createElement("li");
+// sixthli.textContent="item6";
+// ul.append(sixthli);
+// console.log(ul);
+
+
+// static list
+const listitems = document.querySelectorAll(".todo-list ");
+
+
+const sixthli = document.createElement("li");
+sixthli.textContent="item6";
+const ul = document.querySelector(".todo-list ");
+ul.append(sixthli);
+console.log(listitems);
+
