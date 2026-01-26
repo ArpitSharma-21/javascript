cc636c3aa4668271282e2b54212391e0e9e5da70
ArpitSharma-21 <arpi66.ascii@gmail.com>
2026-01-26 21:56:18 +0530

    intro to events

---
+++ 120.js
@@ -0,0 +1,9 @@
+// intro to events
+// click 
+// event add karne ke 3 tarike hai 
+// 1.) 
+
+const btn = document.querySelector(".btn-headline");
+btn.addEventListener("click",()=>{
+    console.log("you hit me !!!!!");
+})
