48b312250084f1e2274b729917dd8d056cdbc0d3
ArpitSharma-21 <arpi66.ascii@gmail.com>
2026-01-27 17:14:56 +0530

    Eventlistner

---
+++ 122.js
@@ -0,0 +1,18 @@
+
+const  allbuttons = document.querySelectorAll(".my-buttons button");
+
+// //for of loop
+// for(let button of allbuttons){
+//   button.addEventListener("click",function(){
+//     console.log(this);
+//   })
+// }
+
+// 
+
+// for(let i=0;i<allbuttons.length;i++){
+//     allbuttons[i].addEventListener("click",function(){
+//         console.log(this);
+//     })
+// }
+
