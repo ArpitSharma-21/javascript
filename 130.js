114803a08ebe67fab045d7793425612238488873
ArpitSharma-21 <arpi66.ascii@gmail.com>
2026-02-01 10:52:18 +0530

    settimeout and cleartimeout

---
+++ 130.js
@@ -0,0 +1,29 @@
+// SETTIMEOUT
+
+// console.log("script start");
+
+// setTimeout(()=>{
+//     console.log("insidesettimeout");
+// },1000);
+
+
+// console.log("script end");
+
+
+//CLEARTIMEOUT
+
+console.log("script start");
+
+const id=setTimeout(()=>{
+    console.log("insidesettimeout");
+},1000);
+
+for(let i=0;i<100;i++){
+    console.log("....");
+}
+
+
+
+console.log("settimeout id ",id);
+clearTimeout(id);
+console.log("script end");
