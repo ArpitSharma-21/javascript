9e11966493e20c6334a0ed773db2eebf98a46ee9
ArpitSharma-21 <arpi66.ascii@gmail.com>
2026-02-01 10:59:25 +0530

    set Interval

---
+++ 131.js
@@ -0,0 +1,19 @@
+const body = document.body;
+const button = document.querySelector("button");
+
+const id = setInterval(() => {
+    const red = Math.floor(Math.random() * 126);
+    const green = Math.floor(Math.random() * 126);
+    const blue = Math.floor(Math.random() * 126);
+
+    const rgb = `rgb(${red}, ${green}, ${blue})`; // ✅ FIX
+    body.style.background = rgb;
+
+}, 1000);
+
+button.addEventListener("click", () => {
+    clearInterval(id);
+    button.textContent = body.style.background;
+});
+
+console.log(id);
